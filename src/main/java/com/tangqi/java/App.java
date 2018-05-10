d9ac1f96340901f7480e4cbaa464a924261557bd
唐琦 <tangqi@163.com>
2018-05-10 16:47:40 +0800

    项目初始化

---
+++ src/main/java/com/tangqi/java/App.java
@@ -0,0 +1,13 @@
+package com.tangqi.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/tangqi/java/factoryPattern/package-info.java
@@ -0,0 +1,1 @@
+package com.tangqi.java.factoryPattern;
